64f2891cad90ae914c14035e95f15d7ea76df92c
Vladimir Kolbas <vladimir.kolbas@xeetech.com>
2016-04-07 14:10:50 +0200

    Initial commit

---
+++ WebPIntegration/ViewController.swift
@@ -0,0 +1,119 @@
+//
+//  ViewController.swift
+//  WebPIntegration
+//
+//  Created by Vladimir Kolbas on 4/7/16.
+//  Copyright © 2016 Vladimir Kolbas. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            imageView.contentMode = .ScaleAspectFit
+        }
+    }
+
+    private let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+    private var dataTask: NSURLSessionDataTask?
+    private var downloadTask: NSURLSessionDownloadTask?
+    
+    // MARK: - Actions
+    
+    /// Fetch webP from the web and display it.
+    @IBAction func fetchWebPAction(sender: AnyObject) {
+        // Cancel task, if it exists
+        dataTask?.cancel()
+        downloadTask?.cancel()
+        
+        let urlString = "http://www.gstatic.com/webp/gallery/1.webp"
+        guard let url = NSURL(string: urlString) else { return }
+        
+        dataTask = session.dataTaskWithURL(url, completionHandler: { [weak self] (data, _, _) in
+            dispatch_async(dispatch_get_main_queue()) {
+                let image = UIImageWithWebPData(data!)
+                self?.setImageWithImage(image)
+            }
+            })
+        
+        dataTask?.resume()
+    }
+    
+    /// Fetch remote JPEG, convert to webP and set image
+    @IBAction func fetchWebJPEGAction(sender: AnyObject) {
+        // Cancel tasks, if it exists
+        dataTask?.cancel()
+        downloadTask?.cancel()
+        
+        let alertController = UIAlertController(title: "Loading Jpeg from the web", message: "Please wait...", preferredStyle: .Alert)
+        presentViewController(alertController, animated: true, completion: nil)
+        
+        let urlString = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_March_2010-1.jpg"
+        guard let url = NSURL(string: urlString) else { return }
+        
+        // Download the JPEG from web
+        downloadTask = session.downloadTaskWithURL(url, completionHandler: { [weak self] (localURL, _, _) in
+            guard let jpegData = NSData(contentsOfURL: url) else { return }
+            guard let jpegImage = UIImage(data: jpegData) else { return }
+            
+            // get webP data from JPEG for saving to cache?
+            let webpData = UIImageWebPRepresentation(jpegImage)
+            
+            // get webP from data for displaying
+            let webpImage = UIImageWithWebPData(webpData)
+            
+            // Possibly save webpImage to cache, and display it
+            dispatch_async(dispatch_get_main_queue()) {
+                self?.dismissViewControllerAnimated(true, completion: nil)
+                self?.setImageWithImage(webpImage)
+            }
+        })
+        
+        downloadTask?.resume()
+    }
+    
+    /// Load local PNG, convert to webP and display
+    @IBAction func localPNGAction(sender: AnyObject) {
+        // Cancel tasks, if it exists
+        dataTask?.cancel()
+        downloadTask?.cancel()
+        
+        let alertController = UIAlertController(title: "Loading and\nconverting 25Mb PNG", message: "Please wait...", preferredStyle: .Alert)
+        presentViewController(alertController, animated: true, completion: nil)
+        
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // Load huge local png
+            let imagePNG = UIImage(named: "galaxy.png")
+            
+            var documents = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+            documents = documents.stringByAppendingString("image.webp")
+
+            // Save webP to disk (For FSNCachemanager or something similar...)
+            let imagePNGData = UIImageWebPRepresentation(imagePNG)
+            
+            imagePNGData.writeToFile(documents, atomically: true)
+            
+            // Load webP from disk
+            guard let imageWebPData = NSData(contentsOfFile: documents) else { return }
+            
+            let imageWebP = UIImageWithWebPData(imageWebPData)
+            
+            dispatch_async(dispatch_get_main_queue()) {
+                self.dismissViewControllerAnimated(true, completion: nil)
+                self.setImageWithImage(imageWebP)
+            }
+        }
+    }
+    
+    // MARK: - Setting image
+    
+    private func setImageWithImage(image: UIImage) {
+        imageView.image = image
+    }
+}
+
