82623acb8a4ab845e2da57ad327833315fcf003d
Vladimir Kolbas <vladimir.kolbas@xeetech.com>
2016-04-20 13:02:07 +0200

    Fixed typo

--- WebPIntegration/ViewController.swift
+++ WebPIntegration/ViewController.swift
@@ -55,16 +55,22 @@ final class ViewController: UIViewController {
 //        let urlString = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_March_2010-1.jpg"
 //        let urlString = "http://res.freestockphotos.biz/pictures/2/2706-outdoor-portrait-of-a-beautiful-teen-girl-pv.jpg"
 //        let urlString = "http://www.inet.hr/~vkolbas/img.jpg"
-        let urlString = "http://lorempixel.com/568/1136"
+        let urlString = "http://e2ua.com/data/wallpapers/69/WDF_1143505.jpg"
         
         guard let url = NSURL(string: urlString) else { return }
         
         // Download the JPEG from web
         downloadTask = session.downloadTaskWithURL(url, completionHandler: { [weak self] (localURL, _, _) in
             print("Done")
-            guard let jpegData = NSData(contentsOfURL: localURL!) else { print("No data"); return }
-            guard let jpegImage = UIImage(data: jpegData) else { return }
-            print("AFtert done")
+            guard let jpegData = NSData(contentsOfURL: localURL!) else {
+                print("No data")
+                return
+            }
+            guard let jpegImage = UIImage(data: jpegData) else {
+                print("Can't convert to JPEG")
+                return
+            }
+            print("After done")
             // get webP data from JPEG for saving to cache?
             let webpData = UIImageWebPRepresentation(jpegImage, .PicturePreset, 50.0, nil)
             print (jpegData.length / 1024, webpData.length / 1024 )
